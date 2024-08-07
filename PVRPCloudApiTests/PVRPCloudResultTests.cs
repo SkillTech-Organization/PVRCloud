dd7b725c3456ee76f4415c18771d2d6f1e7a4846
Tamás Kis <kis6724@gmail.com>
2024-08-07 19:50:55 +0200

    #58 pvrpcloudresult tests

---
+++ PVRPCloudApiTests/PVRPCloudResultTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PVRPCloudApiTests;
+
+public class PVRPCloudResultTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    private const string Endpoint = "/v1/PVRPCloudResult/";
+
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task PVRPCloudResult_ExistingId_ReturnsOk()
+    {
+        string requestId = "12345678";
+
+        var response = await _client.GetAsync(Endpoint + requestId);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task PVRPCloudResult_NonExistingId_ReturnsNotFound()
+    {
+        string requestId = "1234567";
+
+        var response = await _client.GetAsync(Endpoint + requestId);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
