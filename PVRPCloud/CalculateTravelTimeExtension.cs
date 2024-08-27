856828a837d4432aba20de2b01f4ecaedcad0d09
Tamás Kis <kis6724@gmail.com>
2024-08-27 20:22:34 +0200

    #101 calculate travel time

---
+++ PVRPCloud/CalculateTravelTimeExtension.cs
@@ -0,0 +1,21 @@
+using PMapCore.BO;
+using PVRPCloud.Requests;
+
+namespace PVRPCloud;
+
+public static class CalculateTravelTimeExtension
+{
+    public static int CalculateTravelTime(this boRoute route, TruckType truckType)
+    {
+        ArgumentNullException.ThrowIfNull(route);
+
+        List<int> times = new(route.Edges.Count);
+        foreach (var edge in route.Edges)
+        {
+            int value = (int)(edge.EDG_LENGTH / (truckType.SpeedValues[edge.RDT_VALUE] / 3.6 * 60));
+            times.Add(value);
+        }
+
+        return times.Sum();
+    }
+}
--- PVRPCloud/ProblemFile/RelationsRenderer.cs
+++ PVRPCloud/ProblemFile/RelationsRenderer.cs
@@ -1,18 +1,22 @@
 using System.Text;
+using PVRPCloud.Requests;
 
 namespace PVRPCloud.ProblemFile;
 
 public sealed class RelationsRenderer
 {
     private readonly StringBuilder _sb = new();
+    private readonly IReadOnlyList<TruckType> _truckTypes;
     private readonly IReadOnlyDictionary<string, int> _truckTypeIds;
     private readonly List<(ClientNodeIdPair From, ClientNodeIdPair To)> _clientNodes;
     private readonly IReadOnlyDictionary<string, int> _clientIds;
 
-    public RelationsRenderer(IReadOnlyDictionary<string, int> truckTypeIds,
+    public RelationsRenderer(IReadOnlyList<TruckType> truckTypes,
+                             IReadOnlyDictionary<string, int> truckTypeIds,
                              List<(ClientNodeIdPair From, ClientNodeIdPair To)> clientNodes,
                              IReadOnlyDictionary<string, int> clientIds)
     {
+        _truckTypes = truckTypes;
         _truckTypeIds = truckTypeIds;
         _clientNodes = clientNodes;
         _clientIds = clientIds;
@@ -22,16 +26,20 @@ public sealed class RelationsRenderer
     {
         foreach (var (from, to) in _clientNodes)
         {
-            foreach (var (truckTypeId, truckTypePvrpId) in _truckTypeIds)
+            foreach (var truckType in _truckTypes)
             {
+                var truckTypePvrpId = _truckTypeIds[truckType.ID];
+
                 var route = routes
-                    .Where(x => x.fromNOD_ID == from.NodeId && x.toNOD_ID == to.NodeId && x.TruckTypeId == truckTypeId)
+                    .Where(x => x.fromNOD_ID == from.NodeId && x.toNOD_ID == to.NodeId && x.TruckTypeId == truckType.ID)
                     .Single();
 
                 int fromClientId = _clientIds[from.Identifable.ID];
                 int toClientId = _clientIds[to.Identifable.ID];
 
-                _sb.AppendLine($"setRelationAccess({truckTypePvrpId}, {fromClientId}, {toClientId}, {route.route?.DST_DISTANCE ?? 0}, {""})");
+                int time = route.route!.CalculateTravelTime(truckType);
+
+                _sb.AppendLine($"setRelationAccess({truckTypePvrpId}, {fromClientId}, {toClientId}, {route.route?.DST_DISTANCE ?? 0}, {time})");
             }
         }
 
