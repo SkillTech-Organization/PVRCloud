0981856db9e6e2ad1de4dde701db83f0d8aafb0f
Tamás Kis <kis6724@gmail.com>
2024-08-31 15:36:23 +0200

    #110 Depot and Client inherit from ClientBase

--- PVRPCloud/Requests/Client.cs
+++ PVRPCloud/Requests/Client.cs
@@ -1,10 +1,9 @@
 namespace PVRPCloud.Requests;
 
-public sealed class Client : IIdentifiable
+public sealed class Client : ClientBase
 {
-    public string ID { get; init; } = string.Empty;
     public string ClientName { get; init; } = string.Empty;
-    public double Lat { get; init; }
-    public double Lng { get; init; }
     public int ServiceFixTime { get; init; }
+
+    public override string Name => ClientName;
 }
---
+++ PVRPCloud/Requests/ClientBase.cs
@@ -0,0 +1,10 @@
+namespace PVRPCloud.Requests;
+
+public abstract class ClientBase : IIdentifiable
+{
+    public string ID { get; init; } = string.Empty;
+    public double Lat { get; init; }
+    public double Lng { get; init; }
+
+    public abstract string Name { get; }
+}
