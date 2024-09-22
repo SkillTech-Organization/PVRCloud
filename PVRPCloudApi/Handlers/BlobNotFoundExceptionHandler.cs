1b7cc098e9537cecb97fb061fa3b74a8195eaf28
Tamás Kis <kis6724@gmail.com>
2024-09-22 14:55:41 +0200

    #132 Exception logging

--- PVRPCloudApi/Handlers/BlobNotFoundExceptionHandler.cs
+++ PVRPCloudApi/Handlers/BlobNotFoundExceptionHandler.cs
@@ -1,11 +1,21 @@
 using System.Net;
 using Azure;
 using Microsoft.AspNetCore.Diagnostics;
+using PVRPCloud;
 
 namespace PVRPCloudApi.Handlers;
 
+using static LogPvrpExtension;
+
 public class BlobNotFoundExceptionHandler : IExceptionHandler
 {
+    private readonly ILogger<BlobNotFoundExceptionHandler> _logger;
+
+    public BlobNotFoundExceptionHandler(ILogger<BlobNotFoundExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
     public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
         if (exception is not RequestFailedException requestFailedException)
@@ -15,6 +25,8 @@ public class BlobNotFoundExceptionHandler : IExceptionHandler
             return ValueTask.FromResult(false);
 
         httpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+
+        _logger.LogInformation(LogTemplate, "API", "unknown", LogStatus.Exception, requestFailedException.Message);
 
         return ValueTask.FromResult(true);
     }
--- PVRPCloudApi/Handlers/GeneralExceptionHandler.cs
+++ PVRPCloudApi/Handlers/GeneralExceptionHandler.cs
@@ -4,11 +4,19 @@ using PVRPCloud;
 
 namespace PVRPCloudApi.Handlers;
 
+using static LogPvrpExtension;
+
 public static class GeneralExceptionHandler
 {
+    private class ExceptionHandler;
+
     public static async Task HandleAsync(HttpContext context)
     {
         var exceptionHandlerFeature = context.Features.GetRequiredFeature<IExceptionHandlerPathFeature>();
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<ExceptionHandler>>();
+
+        logger.LogInformation(LogTemplate, "API", "unknown", LogStatus.Exception, exceptionHandlerFeature.Error);
 
         context.Response.StatusCode = StatusCodes.Status500InternalServerError;
         await context.Response.WriteAsJsonAsync(new Response()
