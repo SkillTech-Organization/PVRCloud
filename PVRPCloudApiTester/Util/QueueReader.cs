faca166ad8fd715d1981bbd24d36c9d25f4f0c16
RichardVigh <killuaz1739@gmail.com>
2025-12-04 11:35:16 +0100

    SkillTech-Organization/PVRCloud#218: refactor SkillTech-Organization/PVRCloud#215: generic result

--- PVRPCloudApiTester/Util/QueueReader.cs
+++ PVRPCloudApiTester/Util/QueueReader.cs
@@ -12,7 +12,7 @@ internal class GetResultResponse
 {
     public QueueResponse Result { get; set; }
 
-    public List<Result> PVRPCloudResults { get; set; }
+    public List<Result<Project, ProjectRes>> PVRPCloudResults { get; set; }
 
     public bool NoMoreMessages { get; set; } = false;
 
